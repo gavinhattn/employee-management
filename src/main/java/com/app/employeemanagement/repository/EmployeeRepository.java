4125d77590b65f7dc5f921181ee49b0b4d2ca816
WINDOWS-1JQV1IC\gavin <gavinhatton90@googlemail.com>
2024-08-07 18:36:02 +0100

    Added employee entity and EmployeeRepository interface for test to pass

---
+++ src/main/java/com/app/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.app.employeemanagement.repository;
+
+import com.app.employeemanagement.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
--- src/test/java/com/app/employeemanagement/EmployeeRepositoryTest.java
+++ src/test/java/com/app/employeemanagement/EmployeeRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.app.employeemanagement;
 
+import com.app.employeemanagement.repository.EmployeeRepository;
+import com.app.employeemanagement.entity.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
