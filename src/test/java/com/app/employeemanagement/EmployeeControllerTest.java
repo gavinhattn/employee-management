94f3330a2fc3aa4b3e86e961dc92977b00577284
WINDOWS-1JQV1IC\gavin <gavinhatton90@googlemail.com>
2024-08-07 19:48:21 +0100

    Added Red EmployeeController test for getting all employees

---
+++ src/test/java/com/app/employeemanagement/EmployeeControllerTest.java
@@ -0,0 +1,32 @@
+package com.app.employeemanagement;
+
+import com.app.employeemanagement.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Collections;
+
+@WebMvcTest(EmployeeController.class)
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Test
+    public void testGetAllEmployees() throws Exception {
+        Mockito.when(employeeService.getAllEmployees()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(0));
+    }
+}
